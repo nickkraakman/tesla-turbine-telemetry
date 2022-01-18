534226092940afbc0b29e8ab69ea2b121082151b
Nick Kraakman <nickkraakman@gmail.com>
2022-01-18 14:57:01 +0100

    Save and load settings using localstorage

--- js/app.js
+++ js/app.js
@@ -1,13 +1,71 @@
 $(function() 
 {
-    // Open / close settings
+    /**
+     * Open / close settings
+     */
     $( "#settings-btn" ).on( "click", function() {
-      $( ".sheet-overlay" ).addClass( "sheet-overlay-active" )
-      $( ".sheet" ).addClass( "sheet-active" )
+        $( ".sheet-overlay" ).addClass( "sheet-overlay-active" )
+        $( ".sheet" ).addClass( "sheet-active" )
     })
 
     $( ".sheet-overlay, #settings-close-btn" ).on( "click", function() {
-      $( ".sheet-overlay" ).removeClass( "sheet-overlay-active" )
-      $( ".sheet" ).removeClass( "sheet-active" )
+        $( ".sheet-overlay" ).removeClass( "sheet-overlay-active" )
+        $( ".sheet" ).removeClass( "sheet-active" )
     })
+
+
+    /**
+     * Handle settings form changes
+     */ 
+    function saveSettings()
+    {
+        var  formData = []
+
+        // Remove old formData
+        localStorage.removeItem('settings')
+
+        // Loop through form input fields
+        $("#settings-form input[type=number], #settings-form select, #settings-form input[type=radio]:checked").each(function() {
+            formData.push({ 
+                name: this.name, 
+                value: $(this).attr('type') === "radio" ? this.id : this.value,
+                type: $(this).attr('type')
+            })
+        })
+
+        // Convert array to JSON and story in localStorage
+        localStorage.settings = JSON.stringify(formData)
+    }
+
+    function loadSettings()
+    {
+        if (localStorage.settings != undefined) {
+            settings  = JSON.parse(localStorage.settings)
+            for (var i = 0; i < settings.length; i++) {
+                if (settings[i].type != undefined && settings[i].type == "radio")
+                {
+                    $("[name=" + settings[i].name + "]").prop('checked', true)
+                } else {
+                    $("[name=" + settings[i].name + "]").val(settings[i].value)
+                }
+            }
+        } else {
+
+        }
+    }
+    
+    // Get form data from localstorage on page load
+    loadSettings()
+
+    // Listen for changes in the settings form
+    let timeout = 0;
+    
+    $("#settings-form input, #settings-form select").on( "input", function() {
+        // Don't save settings on every change immediately, but wait a little to batch them
+        clearTimeout(timeout);
+
+        timeout = setTimeout(function() {
+            saveSettings()
+        }, 3000)
+    });
 })
