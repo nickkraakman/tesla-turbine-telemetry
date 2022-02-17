df6ac5d1fd8df8f3899dcc5b1bdf54d3dfaffef2
Nick Kraakman <nickkraakman@gmail.com>
2022-02-17 16:00:38 +0100

    Use pigpio to read MEAS sensor

--- m3200.py
+++ m3200.py
@@ -1,232 +1,91 @@
 """Methods to read data from a MEAS M32JM pressure & temperature sensor"""
 
-import smbus  # Alternative is pigpio: http://abyz.me.uk/rpi/pigpio/python.html#i2c_open
 import time
+import pigpio
 import RPi.GPIO as GPIO
 
-# Define I2C communication variables
-bus1 = smbus.SMBus(1)  # Original I2C bus
-bus2 = smbus.SMBus(3)  # Custom I2C bus, where GPIO 23 = SDA and GPIO 24 = SCL
-address = 0x28
-read_header = 0x51
+pi = pigpio.pi()
 
-SDA1_PIN = 2
-SCL1_PIN = 3
-SDA2_PIN = 23
-SCL2_PIN = 24
+if not pi.connected:
+    print("No Pi found")
+    exit()
 
-# Variables from example code
-temp = []  # Array of 7 unsigned 8 bit integers
-Tscope = Pscope = Tdisplay = Pdisplay = float(0)
-Lmax = float(100)  # Span 100L，Zero 0L, Span should be defined by the sensor pressure range. 100 means pressure range of 100L
-Lmin = float(0)
-Pvalue = Tvalue = Pspan = Tspan = 0
-P1 = 1000
-P2 = 15000
-READ_Sensor_SDA = True
+temp = []
 I2C_ERR = 0
+temperature = pressure = float(0)
+Pmax = float(100)  # 100 PSI, should be defined by the sensor pressure range.
+Pmin = float(0)
+Tscope = 200  # -50~150
+Tspan = 2048  # 11 bit
+Pvalue = Tvalue = 0
+P1 = 1000  # 1000 counts is 0% pressure = 0 psi = 0 bar
+P2 = 15000  # 15000 counts is 100% pressure = 100 psi = 7 bar
+Pspan = P2 - P1
+
+SENSOR_ADDRESS = 0x28  # 0101000-
+#read_header = 0x51  # 01010001
+read_bytes = 4
+
+handle1 = pi.i2c_open(1, SENSOR_ADDRESS)  # Original I2C bus 1
+handle2 = pi.i2c_open(3, SENSOR_ADDRESS)  # Custom I2C bus 3, where GPIO 23 = SDA and GPIO 24 = SCL
+
+# Check if we're properly connected to the sensor
+if (handle1 < 0):
+    print('Error connecting to the sensor')
+    pi.stop()
+    exit()
+
+# Wake up from sleep mode
+pi.i2c_write_quick(handle1, 1)  # Send READ_MR command to start measurement and DSP calculation cycle
+time.sleep(2 / 1000)  # Response time from power on to reading measurement data is 8.4 ms with sleep mode
+
+count, data = pi.i2c_read_device(handle1, read_bytes)  # Send READ_DF4 command, to fetch 2 pressure bytes & 2 temperature bytes
+
+pi.i2c_close(handle1)
+pi.stop()
+
+print('count = ', count)
+print('data = ', data)
+
+if count < 0:
+    print('Error reading from the sensor')
+    pi.stop()
+    exit()
 
 
-GPIO.setmode(GPIO.BCM) # Use Broadcom GPIO pin numbering
-#GPIO.setup(SCL1_PIN, GPIO.OUT, pull_up_down=GPIO.PUD_OFF)
-#GPIO.setup(SCL2_PIN, GPIO.OUT, pull_up_down=GPIO.PUD_OFF)
+"""
+data[0] =  10000100  # First two bits are status bits
+data[1] =  00011000
+data[2] =  01011100
+data[3] =  01110000  # Last 5 bits are unrelated and should be ignored
+"""
+print('data[0] =', '{:08b}'.format(data[0]) )
+print('data[1] =', '{:08b}'.format(data[1]) )
+print('data[2] =', '{:08b}'.format(data[2]) )
+print('data[3] =', '{:08b}'.format(data[3]) )
+
+statusCode = '{:08b}'.format(data[0])[:2]
+print('Status code =', statusCode)
+
+if statusCode == '00':
+    print("Status: Normal operation")
+elif statusCode == '01':
+    print("Status: Reserved")
+elif statusCode == '10':
+    print("Status: Stale data")
+else:
+    print("Status: Fault detected")
 
 
-# Switch SDA pin from input to output?  >> input = ALT0, output = OUTPUT
-# Do we even have to do this? Or can we just call smbus.write_byte()?? 
-# But that only sets SDA, how do we then set SCL pin HIGH/LOW?
-# https://raspberrypi.stackexchange.com/a/105637/144210
-# https://forums.raspberrypi.com/viewtopic.php?t=190604#p1198566
-# https://forums.raspberrypi.com/viewtopic.php?t=76849#p549112
-def SDA_IN():
-    """Set SDA pin to I2C INPUT with no pullup"""
-    GPIO.setup(SDA1_PIN, GPIO.I2C, pull_up_down=GPIO.PUD_OFF)
-    #GPIO.setup(SDA2_PIN, GPIO.I2C, pull_up_down=GPIO.PUD_OFF)
+Pvalue = (data[0] << 8) | data[1]
+Tvalue = (data[2] << 3) | (data[3] >> 5)
 
+print('Pvalue = ', Pvalue)
+print('Tvalue = ', Tvalue)
 
-def SDA_OUT():
-    """Set SDA pin to OUTPUT with no pullup"""
-    GPIO.setup(SDA1_PIN, GPIO.OUT, pull_up_down=GPIO.PUD_OFF)
-    #GPIO.setup(SDA2_PIN, GPIO.OUT, pull_up_down=GPIO.PUD_OFF)
+temperature = Tvalue * Tscope / Tspan - 50
+pressure = (Pvalue + 1000) * (Pmax - Pmin) / Pspan + Pmin  # 100L
 
-
-def SCL_High():
-    GPIO.output(SCL1_PIN, GPIO.HIGH)
-    #GPIO.output(SCL2_PIN, GPIO.HIGH)
-
-def SCL_Low():
-    GPIO.output(SCL1_PIN, GPIO.LOW)
-    #GPIO.output(SCL2_PIN, GPIO.LOW)
-
-def SDA_High():
-    GPIO.output(SDA1_PIN, GPIO.HIGH)
-    #GPIO.output(SDA2_PIN, GPIO.HIGH)
-
-def SDA_Low():
-    GPIO.output(SDA1_PIN, GPIO.LOW)
-    #GPIO.output(SDA2_PIN, GPIO.LOW)
-
-
-def delay_us(microseconds):
-    time.sleep(microseconds / 1000000.0)
-
-
-def IIC_Start():
-    """A HIGH to LOW transition on the SDA line while SCL is HIGH"""
-    SDA_OUT()
-
-    SDA_High()
-    SCL_High()
-
-    delay_us(4)
-
-    SDA_Low()
-
-    delay_us(4)
-
-    SCL_Low()
-
-
-def IIC_Stop():
-    """A LOW to HIGH transition on the SDA line while SCL is HIGH"""
-    SDA_OUT()
-
-    SCL_Low()
-    SDA_Low()
-
-    delay_us(4)
-
-    SCL_High()
-    SDA_High()
-
-    delay_us(4)
-
-
-def IIC_Wait_Ack():
-    ucErrTime = 0
-    SDA_IN()
-    SDA_High
-    delay_us(1)
-    SCL_High()
-    delay_us(1)
-    while READ_Sensor_SDA:
-        ucErrTime = ucErrTime + 1
-        if ucErrTime > 250:
-            IIC_Stop()
-            return 1
-    SCL_Low()
-    return 0
-
-
-def IIC_Ack():
-    SCL_Low()
-
-    SDA_OUT()
-
-    SDA_Low()
-
-    delay_us(2)
-
-    SCL_High()
-
-    delay_us(2)
-
-    SCL_Low()
-
-
-def IIC_NAck():
-    SCL_Low()
-
-    SDA_OUT()
-
-    SDA_High()
-
-    delay_us(2)
-
-    SCL_High()
-
-    delay_us(2)
-
-    SCL_Low()
-
-
-def IIC_Send_Byte(txd):
-    SDA_OUT()
-    SCL_Low()
-
-    for t in range(8):
-        if txd & 0x80:  # 0x80 is write?
-            SDA_High()
-        else:
-            SDA_Low()
-
-        txd <<= 1
-
-        delay_us(2)
-        SCL_High()
-        delay_us(2)
-        SCL_Low()
-        delay_us(2)
-
-
-def IIC_Read_Byte(ack):
-    """Should we use bus1.read_byte_data(address, 1) instead???"""
-    receive = 0
-    SDA_IN()
-    for i in range(8):
-        SCL_Low()
-        delay_us(2)
-        SCL_High()
-        receive <<= 1
-        if (READ_Sensor_SDA):
-            receive = receive + 1
-        delay_us(1)
-
-    if not ack:
-        IIC_NAck()
-    else:
-        IIC_Ack()
-    
-    return receive
-
-
-def Get_I2CValue(dataType):
-    """Read sensor value
-    
-    Args:
-        dataType (string): Whether to return 'temperature' or 'pressure'
-
-    Returns: 
-        float: The temperature or pressure
-    """
-    # Wake_up，if non-sleep mode this part is no needed.
-    IIC_Start()  # MR command
-    IIC_Send_Byte(read_header)
-    IIC_Wait_Ack()
-    IIC_Stop()
-    delay_us(2000)  # 2ms
-    IIC_Start()
-    IIC_Send_Byte(read_header)
-    IIC_Wait_Ack()
-    temp[0] = IIC_Read_Byte(1)
-    temp[1] = IIC_Read_Byte(1)
-    temp[2] = IIC_Read_Byte(1)
-    temp[3] = IIC_Read_Byte(0)
-    IIC_Stop()
-
-    if (temp[0] & 0xc0) is 0x00:
-        Pvalue = (temp[0] << 8) | temp[1]
-        Tvalue = (temp[2] << 3) | (temp[3] >> 5)
-        I2C_ERR = 0
-    else:
-        I2C_ERR = 1    
-        
-    Tscope = 200  # -50~150
-    Tspan = 2048  # 11 bit
-
-    if I2C_ERR is 0:
-        Pspan = P2 - P1
-        Tdisplay = Tvalue * Tscope / Tspan - 50
-        Pdisplay = Pvalue * (Lmax - Lmin) / Pspan + Lmin  # 100L
-
-    return Tdisplay if dataType is 'temperature' else Pdisplay
+print('Temperature =', temperature, 'ºC')  # Temperature in ºC
+print('Pressure =', pressure, 'PSI')  # Pressure in PSI (atmospheric is 14.69)
+exit()
